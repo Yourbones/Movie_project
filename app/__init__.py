ab7dcde089f95e9efd987f319a68211824bebf51
Everglow <xuhaoxing9@163.com>
2018-01-31 21:00:21 +0800

    搭建微电影网站前台相关页面

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 #coding:utf-8
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.debug = True
@@ -9,4 +9,9 @@ from app.admin import admin as admin_blueprint
 
 app.register_blueprint(home_blueprint)
 app.register_blueprint(admin_blueprint, url_prefix="/admin")
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("home/404.html")
 
--- app/admin/views.py
+++ app/admin/views.py
@@ -1,7 +1,16 @@
 #coding:utf-8
 from . import admin
-
+from flask import render_template, redirect, url_for
 
 @admin.route("/")
 def index():
-    return "<h1 style='color:red'>This is admin!</h1>"
+    return render_template("admin/index.html")
+
+@admin.route("/login/")
+def login():
+    return render_template("admin/login.html")
+
+
+@admin.route("/logout/")
+def logout():
+    return redirect(url_for('admin.login'))
